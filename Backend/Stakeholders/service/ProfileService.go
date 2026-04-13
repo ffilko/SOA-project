505da15c78334ea6b257e22ca05444e25fd236c0
Jovan Bozic <jovanbozic2003@gmail.com>
2026-04-13 14:50:59 +0200

    feat: add update method in service

--- Backend/Stakeholders/service/ProfileService.go
+++ Backend/Stakeholders/service/ProfileService.go
@@ -19,3 +19,23 @@ func (service *ProfileService) FindUser(id uuid.UUID) (*model.Profile, error) {
 	}
 	return &profile, nil
 }
+
+func (service *ProfileService) Update(id uuid.UUID, updated *model.Profile) (*model.Profile, error) {
+	profile, err := service.ProfileRepo.FindByUserID(id)
+	if err != nil {
+		return nil, fmt.Errorf("Profile with id %s not found", id)
+	}
+
+	profile.Name = updated.Name
+	profile.Surname = updated.Surname
+	profile.ProfileImage = updated.ProfileImage
+	profile.Description = updated.Description
+	profile.Motto = updated.Motto
+
+	err = service.ProfileRepo.Update(&profile)
+	if err != nil {
+		return nil, fmt.Errorf("Error while updating profile")
+	}
+
+	return &profile, nil
+}
